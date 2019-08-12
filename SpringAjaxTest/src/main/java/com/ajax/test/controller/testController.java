20f7cbe3e82aae4bb4e6b739068c9ec2e4d21598
Mahmoudi med Elamine <a-ek@hotmail.fr>
2019-08-13 00:45:57 +0100

    Reglage de la fonction Ajax du JQuery pour n'importe quel bouton avec réponse

--- SpringAjaxTest/src/main/java/com/ajax/test/controller/testController.java
+++ SpringAjaxTest/src/main/java/com/ajax/test/controller/testController.java
@@ -11,12 +11,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class testController {
 
 
-	@RequestMapping("/test/hello")
+	@RequestMapping("/test/say_hello")
+	@ResponseBody
 	public String helloWorld() {
+		System.out.println("------------helloWorld------------");
 		return "hello";
 	}
 	
-	@RequestMapping("/test/ajax_test")
+	@RequestMapping("/test/ajax_test")		
 	public String testPage() {
 		return "test";
 	}
@@ -24,8 +26,8 @@ public class testController {
 	@RequestMapping("/test/get_time")
 	@ResponseBody
 	public String getServerTime() {
-		System.out.println("------------getServerTime()------------");
-		return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date());
+		System.out.println("------------getServerTime()------------");		
+		return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date())+", Adresse: ";
 	}
 	
 	
