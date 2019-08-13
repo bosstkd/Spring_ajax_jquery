2f985f770e26266f5127ad516dd9d45492f0473a
Mahmoudi med Elamine <a-ek@hotmail.fr>
2019-08-13 01:45:22 +0100

    best action on 2nd day 13/08/2019 01h45 midnight

--- SpringAjaxTest/src/main/java/com/ajax/test/controller/testController.java
+++ SpringAjaxTest/src/main/java/com/ajax/test/controller/testController.java
@@ -11,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class testController {
 
 
+//-------------Ajax respense---------------	
+	
 	@RequestMapping("/test/say_hello")
 	@ResponseBody
 	public String helloWorld() {
@@ -18,17 +20,27 @@ public class testController {
 		return "hello";
 	}
 	
-	@RequestMapping("/test/ajax_test")		
-	public String testPage() {
-		return "test";
-	}
-	
-	@RequestMapping("/test/get_time")
+	@RequestMapping("/test/R_get_time")
 	@ResponseBody
 	public String getServerTime() {
 		System.out.println("------------getServerTime()------------");		
 		return new SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new Date())+", Adresse: ";
 	}
+	
+//-----------------------------------------	
+	
+	@RequestMapping("/test/ajax_test")		
+	public String testPage() {
+		return "test";
+	}
+	
+	@RequestMapping("/")		
+	public String test2Page() {
+		return "test2";
+	}
+	
+
+	
 	
 	
 }
